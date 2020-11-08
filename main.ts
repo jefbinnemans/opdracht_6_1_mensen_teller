c36556343093481ed2603648f5b6f7d93f7e5543
jefbinnemans <41265139+jefbinnemans@users.noreply.github.com>
2020-11-08 12:22:35 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,37 @@
+let aantal_personen = 0
+input.onButtonPressed(Button.A, function () {
+    if (aantal_personen < 25) {
+        aantal_personen = aantal_personen + 1
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+        basic.clearScreen()
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(aantal_personen)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    if (aantal_personen != 0) {
+        aantal_personen = aantal_personen - 1
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+    }
+})
 basic.forever(function () {
-    let aantal_personen = 0
-    if (aantal_personen > 25) {
-        basic.showString("Te veel volk!")
+    if (aantal_personen == 25) {
+        basic.showString("Volzet!")
     }
 })
